13ce83ff8ce5fa5be040d531ccc56bbc65402d29
PillowSky <ihyzi@163.com>
2014-08-28 11:02:38 +0800

    use cooperator

--- twisted/pool.py
+++ twisted/pool.py
@@ -1,5 +1,5 @@
-from twisted.internet import reactor
-from twisted.internet.defer import Deferred, DeferredList
+import time
+from twisted.internet import reactor, task
 from twisted.internet.protocol import Protocol
 from twisted.web.client import Agent, HTTPConnectionPool
 
@@ -7,25 +7,24 @@ baseUrl = 'http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode='
 
 start = 1001
 upLimit = 3000
-current = start
 
 class onBody(Protocol):
-    def __init__(self, deferred):
+    def __init__(self, deferred, i):
         self.deferred = deferred
+        self.i = i
         self.body = ''
 
     def dataReceived(self, bytes):
         self.body += bytes
 
     def connectionLost(self, reason):
-        self._factory.pool.release(None)
-        #print(reason)
-        global current
-        print('No. %s, HTML length: %s ' % (current, len(self.body)))
-        current += 1
+        if str(reason.value) == 'Response body fully received':
+            print('No. %s, HTML length: %s ' % (self.i, len(self.body)))
+        else:
+            print(reason)
         
-def onHeader(response):
-    response.deliverBody(onBody(response))
+def onHeader(response, i):
+    response.deliverBody(onBody(response, i))
     return response
 
 def errorHandler(err):
@@ -34,13 +33,21 @@ def errorHandler(err):
 def requestFactory():
     for i in range(start, upLimit):
         deferred = agent.request('GET', baseUrl + str(i))
-        deferred.addCallback(onHeader)
+        deferred.addCallback(onHeader, i)
         deferred.addErrback(errorHandler)
-        reactor.iterate(1)
+        print('Generated %s' % i)
+        #reactor.iterate(1)
+        yield None
+
+    print(time.time() - startTimeStamp)
+    yield True
 
 pool = HTTPConnectionPool(reactor)
-#pool.maxPersistentPerHost = 5
+pool.maxPersistentPerHost = 5
 agent = Agent(reactor, pool=pool)
 
-reactor.callWhenRunning(requestFactory)
+startTimeStamp = time.time()
+cooperator = task.Cooperator()
+cooperator.cooperate(requestFactory())
+#reactor.callWhenRunning(requestFactory)
 reactor.run()
--- twisted/treq.py
+++ twisted/treq.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 from twisted.internet import epollreactor
-epollreactor.install()
+epollreactor
 from twisted.internet import reactor, task
 import treq
 
 cooperator = task.Cooperator()
-done = 0
-
 baseUrl = 'http://acm.zju.edu.cn/onlinejudge/showProblem.do?problemCode='
+
+start = 1001
+end = 3500
 
 def onHeader(response, i):  
     deferred = treq.text_content(response)
@@ -16,24 +17,19 @@ def onHeader(response, i):
     return deferred
 
 def onBody(body, i):
-    global done
-    done += 1
-    print('Received %s, Length %s, done %s' % (i, len(body), done))
+    print('Received %s, Length %s' % (i, len(body)))
 
 def errorHandler(err):
-    global reactor
     print(err)
-    reactor.stop()
-    exit()
 
 def requestFactory():  
-    global baseUrl, reactor
-    for i in range (1001, 3500):
-        print('Generator %s' % i)
+    for i in range (start, end):
+        print('Generated %s' % i)
         deferred = treq.get(baseUrl + str(i))
         deferred.addCallback(onHeader, i)
         deferred.addErrback(errorHandler)
-        reactor.iterate()
+        
+        #reactor.iterate()
         #reactor.callLater(2,printTime)
         yield None
 
